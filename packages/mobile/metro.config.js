44980b2f7c0def81e3aff70d8cfbb89c51061b19
x0s3 <x0s3.prog@gmail.com>
2019-07-11 23:40:22 +0200

    added mobile app with hermes and 0.60.3 with autolinking android fixed

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,22 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path');
+
+module.exports = {
+  watchFolders: [
+    // To allow finding files outside mobile
+    path.resolve(__dirname, '../..')
+  ],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  }
+};
