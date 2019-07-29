2146b69fda0ae3787a2a85f4050eafbbc9288c3b
x0s3 <x0s3.prog@gmail.com>
2019-07-29 17:59:43 +0200

    - updated android splash screen :)

--- packages/mobile/android/app/src/main/java/com/rxmarket/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/rxmarket/MainActivity.java
@@ -1,15 +1,32 @@
 package com.rxmarket;
 
 import android.graphics.Color;
-import android.view.View;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
     @Override
     protected void addDefaultSplashLayout() {
-        View view = new View(this);
-        view.setBackgroundColor(Color.parseColor("#151A30"));
-        setContentView(view);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setBackgroundColor(Color.parseColor("#151A30"));
+        linearLayout.setGravity(Gravity.CENTER);
+
+        TextView text = new TextView(this);
+        text.setText(R.string.app_name);
+        text.setGravity(Gravity.CENTER);
+        text.setTextColor(Color.parseColor("#F7F9FC"));
+        text.setTextSize(50);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            text.setFontFeatureSettings("sans-serif-medium");
+        }
+        text.setTypeface(null, Typeface.BOLD);
+        linearLayout.addView(text);
+
+        setContentView(linearLayout);
     }
 }
