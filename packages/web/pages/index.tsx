3f07d801cb871a8f2d769ab41a5334f27311a1ee
x0s3 <x0s3.prog@gmail.com>
2019-08-01 15:12:01 +0200

    **WEB** - Started Web Manager App with Nextjs

---
+++ packages/web/pages/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function Home() {
+  const [counter, setCounter] = useState<number>(0);
+
+  return (
+    <div>
+      <h1>Welcome to RxMarket Manager SSR</h1>
+      <h2>Counter {counter}</h2>
+      <button onClick={() => setCounter(c => c + 1)}>INCREMENT</button>
+    </div>
+  );
+}
+
+export default Home;
